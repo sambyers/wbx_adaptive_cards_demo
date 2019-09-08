721808619b356976de5487d9ce205a975da888ed
sambyers <samwbyers@gmail.com>
2019-09-08 14:01:27 -0400

    fixed up resopnse msgs

--- main.py
+++ main.py
@@ -15,9 +15,8 @@ or implied. """
 
 
 from webexteamssdk import WebexTeamsAPI
-from flask import Flask, request
+from flask import Flask, request, Response, jsonify
 from cards import input_card
-from sys import stdout
 
 
 app = Flask(__name__)
@@ -29,6 +28,7 @@ def get_attachments(self, id):
     json_data = self._session.get('/attachment/actions/' + id)
     return json_data
 
+
 @app.route('/', methods=['POST'])
 def index():
     if request:
@@ -38,18 +38,23 @@ def index():
         WebexTeamsAPI.get_attachments = get_attachments
 
         request_json = request.get_json()
-        print(type(request))
-        print(request.args.get('data'))
-        print(type(request_json))
-        print(request_json)
-        stdout.flush()
 
-        if request.args and 'data' in request.args:
-            webhook = request.args.get('data')
-            room_id = webhook['roomId']
+        if request.form and 'data' in request.form:
+            data = request.form.get('data')
+            room_id = data['roomId']
             wbxapi.messages.create(roomId=room_id, text='Oops! Something is broken or you are using the mobile app for which cards are not supported yet.', attachments=input_card)
-            return None
+            response_data = {'message': 'received'}
+            response_data.status_code = 200
+            return response_data
+            
         elif request_json and 'data' in request_json:
             room_id = request_json['data']['roomId']
             wbxapi.messages.create(roomId=room_id, text='Oops! Something is broken or you are using the mobile app for which cards are not supported yet.', attachments=input_card)
-            return None
+            response_data = {'message': 'received'}
+            response_data.status_code = 200
+            return response_data
+
+        else:
+            response_data = {'message': 'request not supported'}
+            response_data.status_code = 200
+            return response_data
